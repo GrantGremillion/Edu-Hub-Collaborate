16031004d83348a391ed974809109e8113e8e733
Beanless <jwo011@email.latech.edu>
2024-01-08 14:59:20 -0600

    minor changes to match updated files setup

--- my-react-app/src/Components/UserProfileHeaderBox.js
+++
@@ -1,8 +0,0 @@
-
-function UserProfileHeaderBox() {
-    return (
-        <h1>User Profile</h1>
-    );
-  }
-  
-  export default UserProfileHeaderBox;
--- my-react-app/src/Pages/UserProfile.js
+++ my-react-app/src/Pages/UserProfile.js
@@ -1,6 +1,7 @@
-import UserProfileHeaderBox from '../Components/UserProfileHeaderBox';
 import * as React from 'react';
 import {Button, Grid} from '@mui/material';
+import HeaderBox from '.././Components/HeaderBox';
+
 
 function UserProfile() {
 
@@ -12,18 +13,17 @@ return (
             alignItems="center"
             justifyContent="center">
             <Grid item xs={1}>
-            <UserProfileHeaderBox>
-            </UserProfileHeaderBox>
+                <HeaderBox text="User Profile" />
             </Grid>
             <Grid item xs={2}>
-            <Button variant="contained" color="primary" size="large"  onClick={() => {alert('would edit profile');}} style={{ width: '200px' }} >
-                Edit Profile
-            </Button>
+                <Button variant="contained" color="primary" size="large"  onClick={() => {alert('would edit profile');}} style={{ width: '200px' }} >
+                    Edit Profile
+                </Button>
             </Grid>
             <Grid item xs={3}>
-            <Button variant="contained" color="primary" size="large"  onClick={() => {alert('opens settings :D');}} style={{ width: '200px' }} >
-                Settings
-            </Button>
+                <Button variant="contained" color="primary" size="large"  onClick={() => {alert('opens settings :D');}} style={{ width: '200px' }} >
+                    Settings
+                </Button>
             </Grid>
         </Grid>
     
