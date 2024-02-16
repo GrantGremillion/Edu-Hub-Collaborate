87dfa1286dd945b6225eb6a9e88ec1b2451c75c8
Jenny Yonjan Tamang <jyo009@email.latech.edu>
2024-02-16 00:05:44 -0600

    Revert "solving merge conflict and added the password verificstion again"
    This reverts commit 13ee0b164e4253a7907ff8dfdecada39cf6f0c83.

--- my-react-app/src/Pages/RecoverPassword.js
+++ my-react-app/src/Pages/RecoverPassword.js
@@ -5,13 +5,9 @@ import PlainNavBar from '../Components/PlainNavBar'; // Assuming this is your cu
 import bg from '../Images/bg.jpg'; // Assuming this is your background image
 // Using the same navigation functionality
 import { useNavigate } from 'react-router-dom';
-<<<<<<< Updated upstream
 // used to handle user toggled dark mode
 import dark_bg from '.././Images/dark_bg.jpg';
 import * as themes from '.././Config';
-=======
-import { DARKMODE } from '../Config';
->>>>>>> Stashed changes
 
 function RecoverPassword() {
     const [email, setEmail] = useState('');
@@ -29,7 +25,6 @@ function RecoverPassword() {
         navigate('/Login');
     }
 
-<<<<<<< Updated upstream
     // checks for the theme the page is in, and applys it to these variables
     if (themes.DARKMODE) {
         var containerColor = themes.darkContainer;
@@ -44,25 +39,14 @@ function RecoverPassword() {
         var background = bg;
     }
     
-=======
-    const backgroundImage = DARKMODE ? dark_bg : bg;
-    const containerStyle = DARKMODE ? { background: '#216E6B', marginTop: '75px', height: '400px', marginBottom: '75px' }
-                                    : { background: '#e0f2f1', marginTop: '75px', height: '400px', marginBottom: '75px' };
-
->>>>>>> Stashed changes
     return (
         <div>
             <PlainNavBar />
             <Box
                 className="bg"
                 style={{
-<<<<<<< Updated upstream
                     backgroundImage: `url(${background})`,
                     backgroundSize: "cover",
-=======
-                    backgroundImage: `url(${backgroundImage})`,
-                    backgroundSize: 'cover',
->>>>>>> Stashed changes
                     zIndex: '-1',
                     position: 'fixed',
                     top: 0,
@@ -70,7 +54,6 @@ function RecoverPassword() {
                     right: 0,
                     bottom: 0,
                     width: '100%',
-<<<<<<< Updated upstream
                     height: '100%'
                 }}
             ></Box>
@@ -101,49 +84,14 @@ function RecoverPassword() {
                             fullWidth 
                             variant="contained" 
                             style={{ background: buttonColor, color: textColor }}
-=======
-                    height: '100%',
-                }}
-            ></Box>
-            <Container maxWidth="sm" style={containerStyle}>
-                <Grid container spacing={5} direction="column" alignItems="center" justifyContent="center">
-                    <Grid item xs={12}>
-                        <HeaderBox text={'Recover Your Password'} />
-                    </Grid>
-                    <Grid item xs={12}>
-                        <TextField
-                            fullWidth
-                            id="email-input"
-                            label="Username / Email"
-                            variant="filled"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                        />
-                    </Grid>
-                    <Grid item xs={12}>
-                        <Button
-                            fullWidth
-                            variant="contained"
-                            style={{ background: DARKMODE ? '#009688' : '#b2dfdb', color: 'white' }}
->>>>>>> Stashed changes
                             onClick={handleRequestVerification}
                         >
                             Request Verification Code
                         </Button>
                     </Grid>
-<<<<<<< Updated upstream
 
                     <Grid item xs={1}>
                         <Button variant="contained" size="small"  onClick={handleClickBack} style={{ width: '100px', background: buttonColor, color: textColor}} sx={{fontFamily: 'Courier New', fontSize: 'large', marginTop: '-15%', marginLeft: '-225%'}} >
-=======
-                    <Grid item xs={12}>
-                        <Button
-                            variant="contained"
-                            size="small"
-                            onClick={handleClickBack}
-                            style={{ width: '100px', background: DARKMODE ? '#009688' : '#b2dfdb', color: 'white', marginTop: '15px' }}
-                        >
->>>>>>> Stashed changes
                             Back
                         </Button>
                     </Grid>
@@ -151,10 +99,7 @@ function RecoverPassword() {
             </Container>
         </div>
     );
-<<<<<<< Updated upstream
     
-=======
->>>>>>> Stashed changes
 }
 
 export default RecoverPassword;
