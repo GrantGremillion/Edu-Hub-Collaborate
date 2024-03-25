639b21c298fc2549204225119191682516720bfd
Beanless <jwo011@email.latech.edu>
2024-03-24 20:15:37 -0500

    Changed report issue page to be a text page with information.

--- my-react-app/src/Pages/ReportPage.js
+++ my-react-app/src/Pages/ReportPage.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Button, Grid, Container, TextField, Box } from '@mui/material';
+import { Button, Grid, Container, Box, Typography, Divider } from '@mui/material';
 import HeaderBox from '../Components/HeaderBox';
 import PlainNavBar from '../Components/PlainNavBar';
 import bg from '../Images/bg.jpg';
@@ -9,49 +9,12 @@ import * as themes from '.././Config';
 import { useNavigate } from 'react-router-dom';
 
 function ReportPage() {
-    const [issueReport, setIssueReport] = useState('');
-    const [integrityReport, setIntegrityReport] = useState('');
     const navigate = useNavigate();
 
-    // jeremy added this, this is attempt at email stuff
-    /*
-    const handleRequestVerification = async () => {
-        
-        try {
-            const response = await fetch(`${process.env.REACT_APP_API_URL}/api/send-otp`, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({ email }),
-            });
-
-            const data = await response.json();
-            if (response.ok) {
-                alert(data.message); // "OTP sent successfully."
-            } else {
-                throw new Error(data.error || 'Failed to send email.');
-            }
-        } catch (error) {
-            alert(error.message);
-        }
-    };
-    */
-
-    const handleIssueSubmit = () => {
-        // Logic to send issue report to devs
-        console.log(issueReport);
-        alert('Your issue has been reported to the developers. Redirecting to Home page...');
-        navigate("/");
-    };
-
-    const handleIntegritySubmit = () => {
-        // Logic to send academic integrity violation report to the professor
-        console.log(integrityReport);
-        alert('The violation of academic integrity has been reported. Redirecting to Home page...');
-        navigate("/");
-    };
-
+    const handleClick = () => {
+        // Use navigate to go to the UserProfile page
+        navigate("/Home");
+    }
     // checks for the theme the page is in, and applys it to these variables
     if (themes.DARKMODE) {
         var containerColor = themes.darkContainer;
@@ -84,56 +47,30 @@ function ReportPage() {
                     height: '100%'
                 }}
             ></Box>
-            <Container maxWidth="sm" style={{ background: containerColor, marginTop: '75px', paddingBottom: '75px' }}>
+            <Container maxWidth="sm" height="fit-content" style={{ background: containerColor, marginTop: '75px', paddingBottom: '25px' }}>
                 <Grid container spacing={3} direction="column" alignItems="center" justifyContent="center">
                     <Grid item xs={12}>
                         <HeaderBox text={'Report an Issue or Violation'} />
                     </Grid>
 
                     <Grid item xs={12}>
-                        <TextField
-                            fullWidth
-                            id="issue-report"
-                            label="Report an Issue to Developers"
-                            variant="outlined"
-                            multiline
-                            rows={4}
-                            value={issueReport}
-                            onChange={(e) => setIssueReport(e.target.value)}
-                        />
-                        <Button
-                            fullWidth
-                            variant="contained"
-                            style={{ marginTop: '20px', background: buttonColor, color: textColor }}
-                            onClick={handleIssueSubmit}
-                        >
-                            Report to Developers
-                        </Button>
+                        <Typography fontFamily="Courier New" alignItems="center" justifyContent="center" fontSize="large" marginBottom="25px">
+                            If you encounter any bugs or issues with the website, please do not hesitate to email us at eduhubcollaborate@gmail.com with information on the issue you are having. Also feel free to message us with any ideas for improvements!
+                        </Typography>
+                    
+
+                        <Divider sx={{ borderBottomWidth: 2 }}></Divider>
+
+                    
+                        <Typography fontFamily="Courier New" alignItems="center" justifyContent="center" fontSize="large" marginTop="25px">
+                            Since different schools have different Academic Honesty statements and requirements of their students, it is not realistically within our power to police potential cheating or Academic Dishonesty. Please contact your teacher if you suspect cheating is taking place.
+                        </Typography>
                     </Grid>
 
                     <Grid item xs={12}>
-                        <TextField
-                            fullWidth
-                            id="integrity-report"
-                            label="Report Academic Integrity Violation"
-                            variant="outlined"
-                            multiline
-                            rows={4}
-                            value={integrityReport}
-                            onChange={(e) => setIntegrityReport(e.target.value)}
-                        />
-                        <Button
-                            fullWidth
-                            variant="contained"
-                            style={{ marginTop: '20px', background: buttonColor, color: textColor }}
-                            onClick={handleIntegritySubmit}
-                        >
-                            Report to Administrator
+                        <Button size="small"  onClick={handleClick} style={{ color: textColor, width: '250px', background: buttonColor}} sx={{fontFamily: 'Courier New', fontSize: 'large', marginTop: '0%'}} >
+                            Return to Home
                         </Button>
-                    </Grid>
-
-                    <Grid item xs={12}>
-                        <GoBackButton />
                     </Grid>
                 </Grid>
             </Container>
