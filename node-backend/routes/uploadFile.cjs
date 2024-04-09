e8f2375c5eea16f59f45212af3f37b3077adb4e1
GrantGremillion <grg011@latech.edu>
2024-04-08 19:24:08 -0500

    fixed teacher verification image uploads to keep their file name and type

--- node-backend/routes/uploadFile.cjs
+++ node-backend/routes/uploadFile.cjs
@@ -1,7 +1,17 @@
 
 // multer library allows us to store images on our local machine
 const multer = require('multer')
-const upload = multer({ dest: process.env.UPLOAD_FILE_PATH_TEACHER_IMAGES })
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+      cb(null, process.env.UPLOAD_FILE_PATH_TEACHER_IMAGES) 
+  },
+  filename: function (req, file, cb) {
+      cb(null, file.originalname) 
+  }
+});
+
+const upload = multer({ storage: storage, dest: process.env.UPLOAD_FILE_PATH_TEACHER_IMAGES })
 
 const express = require('express');
 const router = express.Router();
@@ -9,7 +19,7 @@ const db = require('../database.cjs')
 
 ////// Upload File API //////
 
-router.post('/uploadFile', upload.single('image'), (req, res) => {
+router.post('/uploadFile', upload.single("image"), (req, res) => {
   
     const imageName = req.file.filename;
     console.log(imageName);
