00a39e06ebc6b9ca4b4a00465ce9a637fe5aa895
Grant Gremillion <grg011@latech.edu>
2024-04-17 20:03:20 -0500

    Notecards and notecard sets are now stored on the backend

--- node-backend/routes/notecards.cjs
+++ node-backend/routes/notecards.cjs
@@ -6,34 +6,60 @@ const db = require('../database.cjs')
 
 router.post('/create_set', (req,res) => {
 
+
     const name = req.body.name;
     const cards = req.body.cards;
 
     const createNotecardSetSQL = "INSERT INTO NotecardSets (set_name) VALUES (?)";
     const getSetIDSQL = "SELECT Setid FROM NotecardSets ORDER BY Setid DESC LIMIT 1";
-    const createNotecardSQL = "INSERT INTO Notecards (Setid,term,definition) VALUES (?)";
+    let createNotecardSQL = "INSERT INTO Notecards (Setid,term,def) VALUES";
 
     db.query(createNotecardSetSQL, [name], (err) => {
 
         if (err) {
-        return res.status(500).json({ error: err.message });
+            return res.status(500).json({ error: err.message });
         }
 
         // Gets the ID for the notecard set just created
         db.query(getSetIDSQL, (err,data) => {
 
             if (err) {
-            return res.status(500).json({ error: err.message });
+                return res.status(500).json({ error: err.message });
             }
 
-            const setID = data[0].Setid;
+            setID = data[0].Setid;
+
+
+
+            let values = [];
+
+            cards.forEach(card => {
+                const term = card.term;
+                const definition = card.definition;
+                
+                // Push the values into the array
+                values.push(`(${db.escape(setID)}, ${db.escape(term)}, ${db.escape(definition)})`);
+            });
+
+            // Concatenate the values and execute the query
+            if (values.length > 0) {
+                createNotecardSQL += values.join(",");
+                db.query(createNotecardSQL, (err) => {
+                    if (err) {
+                        // Handle error
+                        console.error(err);
+                    } else {
+                        // Query executed successfully
+                        console.log("Cards inserted successfully");
+                    }
+                });
+            } 
+            else {
+                console.log("No cards to insert");
+            }
+            
         });
-
-        for (let i = 0; i < cards.length; i++) {
-            const item = cards[i];
-            console.log(item);
-        }
-
+        
         return res.json({ Status: "Success"});
         
     });
